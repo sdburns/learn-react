9c6a869b8ec8a07f3aa6e50a67ef01f25bd0c71c
Stuart Burns <stuart.burns@psycle.com>
2025-04-15 10:44:21 +0100

    React facts initial HTML

--- 01. Static pages/12. ReactFacts Project - Markup/index.jsx
+++ 01. Static pages/12. ReactFacts Project - Markup/index.jsx
@@ -12,3 +12,21 @@ Hints:
 * You can also set the `width` attribute of the image element
   just like in HTML. In the slide, I have it set to 40px
  */
+
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+  <main>
+    <header><img src="react-logo.png" width="40px" alt="React logo"></img>ReactFacts</header>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K starts on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </main>
+)
