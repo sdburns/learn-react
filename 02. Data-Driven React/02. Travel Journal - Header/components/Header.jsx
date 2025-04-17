771525a47a726c7f754fde47459d3f77bef53c12
Stuart Burns <stuart.burns@psycle.com>
2025-04-17 16:30:30 +0100

    Travel journal header

---
+++ 02. Data-Driven React/02. Travel Journal - Header/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="../globe.png" alt="Globe icon"></img>
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
