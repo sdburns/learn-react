4351541632483fe9ecdd067c50ce6a7e6265f948
Stuart Burns <stuart.burns@psycle.com>
2025-04-17 10:53:04 +0100

    Parent/child components

--- 01. Static pages/18. Custom Components - Parent Child Components/index.jsx
+++ 01. Static pages/18. Custom Components - Parent Child Components/index.jsx
@@ -10,22 +10,40 @@ const root = createRoot(document.getElementById("root"))
  * the Page component where the `header` used to be.
  */
 
+function Header() {
+    return (
+        <header>
+            <img src="react-logo.png" width="40px" alt="React logo" />
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
 function Page() {
     return (
         <>
-            <header>
-                <img src="react-logo.png" width="40px" alt="React logo" />
-            </header>
-            <main>
-                <h1>Reason I am excited to learn React</h1>
-                <ol>
-                    <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                    <li>I am more likely to get a job as a front end developer if I know React</li>
-                </ol>
-            </main>
-            <footer>
-                <small>© 2024 Ziroll development. All rights reserved.</small>
-            </footer>
+            <Header />
+            <MainContent />
+            <Footer />
         </>
     )
 }
